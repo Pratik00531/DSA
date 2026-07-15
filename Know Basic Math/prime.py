ee7a858b3e612047d99dbdde6ad20c883e7c4bc3
Pratik00531 <patelpratik8489@gmail.com>
2026-07-15 23:28:45 +0530

    Day 10

---
+++ Know Basic Math/prime.py
@@ -0,0 +1,10 @@
+print("Enter a number :")
+n = int(input())
+
+
+for i in range (2, n ):
+    if n % i == 0:
+        print("The number is not a prime number")
+        break
+else:
+    print("The number is a prime number")   
