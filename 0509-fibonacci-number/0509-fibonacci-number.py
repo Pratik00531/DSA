2f4072639404711fd6567ad97ed6abb57b80547d
Pratik Patel <126221787+Pratik00531@users.noreply.github.com>
2026-09-15 23:25:47 +0530

    Time: 13 ms (81.11%), Space: 12.3 MB (89.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def fib(self, n):
-        if n==0:
-            return 0
-        if n==1:
-            return 1
+        if n==0 or n == 1:
+            return n
         a,b=0,1
         for _ in range(2,n+1):
             a,b=b,a+b
