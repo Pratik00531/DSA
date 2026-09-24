a8f87411f00b23ca5b72e7931a5bd453a0515282
Pratik Patel <126221787+Pratik00531@users.noreply.github.com>
2026-09-24 23:13:08 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,7 +1,8 @@
 class Solution(object):
     def search(self, nums, target):
+        n = len(nums)
         low = 0
-        high = len(nums) - 1 
+        high = n - 1 
         while low <= high :
             mid = (low + high) // 2
             if nums[mid] == target :
